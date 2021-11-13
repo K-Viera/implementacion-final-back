6c6aefa8a54ca632d13200c9bf4b01ca26b57f24
Kevin <kenvi.009@gmail.com>
2021-11-13 17:28:14 -0500

    fix bugs

--- src/calculators/fibonacci.js
+++ src/calculators/fibonacci.js
@@ -1,12 +1,11 @@
 const fibonacci = {};
 
 fibonacci.calculateFibonacci = (n) => {
-  console.log(n);
-  if ((n = 0)) {
+  if (n == 0) {
     return "";
-  } else if ((n == 1)) {
+  } else if (n == 1) {
     return "0";
-  } else if ((n == 2)) {
+  } else if (n == 2) {
     return "0 1";
   } else {
     var result = "0 1";
